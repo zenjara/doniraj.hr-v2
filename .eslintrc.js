358754b16562495da4e36c0142fab40cb77e4044
zenjara <ivan.matas2@gmail.com>
2022-02-06 20:26:43 +0100

    setup linters

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:mdx/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.mdx'],
+      extends: ['plugin:mdx/overrides'],
+      rules: {
+        'react/jsx-no-undef': 'off',
+      },
+    },
+  ],
+  plugins: ['simple-import-sort', 'eslint-plugin-react'],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
+};
